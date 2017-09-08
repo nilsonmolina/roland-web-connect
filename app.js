3e1684df165f600f47f1c9f64b391d87acd43476
Nilson <MolinaNilson@gmail.com>
2017-09-08 05:42:22 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var BluetoothController = (function() {
+  var dom = {
+    table: document.getElementById('resultsTable'),
+    button: document.getElementById('button'),
+    widget: document.getElementById('widget')
+  };
+
+  var bindEvents = function() {
+    dom.button.addEventListener('click', function() {
+      beginPairing();
+    });
+  };
+    
+  function beginPairing(){
+    navigator.bluetooth.requestDevice({
+      acceptAllDevices: true
+    }).then(device => {
+      // Human-readable name of the device.
+      console.log(device.name);
+
+      // Attempts to connect to remote GATT Server.
+      return device.gatt.connect();
+      })
+    .then(server => { /* ... */ })
+    .catch(error => { console.log(error); });
+  }
+
+  return {
+    init: function() {
+      bindEvents();
+      setTimeout(function(){
+          dom.widget.classList.toggle('hidden');
+      }, 250);
+    }
+  };
+})();
+
+BluetoothController.init();
