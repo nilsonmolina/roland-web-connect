abe6d2a25543193fd0cac92bb6487e5b79c3df6d
Nilson <MolinaNilson@gmail.com>
2017-09-08 22:31:31 -0400

    output results to table

--- app.js
+++ app.js
@@ -18,12 +18,23 @@ var BluetoothController = (function() {
     }).then(device => {
       // Human-readable name of the device.
       console.log(device.name);
+      outputResults(device.name);
       // Attempts to connect to remote GATT Server.
       return device.gatt.connect();
       })
     .then(server => { /* ... */ })
     .catch(error => { console.log(error); });
   }
+    
+  var outputResults = function(result) {
+    var row = dom.table.getElementsByTagName('tbody')[0].insertRow(0);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = new Date().toLocaleString();
+    cell2.innerHTML = result;
+  };
 
   return {
     init: function() {
